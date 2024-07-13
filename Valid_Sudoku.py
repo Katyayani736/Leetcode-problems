cb97642b4eafe992040a4ebb477d7b618f279b66
Katyayani Verma <112198478+Katyayani736@users.noreply.github.com>
2024-07-13 23:59:58 +0530

    Update Valid_Sudoku.py

--- Valid_Sudoku.py
+++ Valid_Sudoku.py
@@ -6,7 +6,6 @@ class Solution:
                 element = board[i][j]
                 if element != '.':
                     res += [(i, element), (element, j), (i // 3, j // 3, element)]
-
         return len(res) == len(set(res))
 
 def main():
