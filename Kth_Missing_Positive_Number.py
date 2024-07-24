d5b9a1ce50c93c86ca2ea568431fdb2130b847a4
Katyayani Verma <112198478+Katyayani736@users.noreply.github.com>
2024-07-24 23:51:39 +0530

    Create Kth_Missing_Positive_Number.py

---
+++ Kth_Missing_Positive_Number.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findKthPositive(self, arr, k):
+        n = len(arr)
+        low = 0
+        high = n - 1
+        while low <= high:
+            mid = (low + high) // 2
+            missing = (arr[mid] - (mid + 1))
+            if missing < k:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return low + k
+
+# Main function for user input and function call
+solution = Solution()
+
+# Get array and k as input
+n = int(input("Enter the number of elements in the array: "))
+arr = []
+for i in range(n):
+    num = int(input())
+    arr.append(num)
+k = int(input("Enter the value of k: "))
+
+result = solution.findKthPositive(arr, k)
+print("The kth positive missing number is:", result)
+
