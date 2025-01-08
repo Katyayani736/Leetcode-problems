988815d99cc28f87cc0728f75147c8cf12537f05
Katyayani Verma <112198478+Katyayani736@users.noreply.github.com>
2025-01-08 21:14:53 +0530

    Create Count_Prefix_and_Suffix_Pairs_I.py

---
+++ Count_Prefix_and_Suffix_Pairs_I.py
@@ -0,0 +1,15 @@
+#3042. Count Prefix and Suffix Pairs I
+class Solution:
+    def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        c=0
+        def isPrefixAndSuffix(str1,str2):
+            if len(str1)>len(str2):
+                return False
+           # print(str2[0:len(str1)],str2[len(str2)-len(str1):len(str2)])
+            if str2[0:len(str1)]==str1 and str2[len(str2)-len(str1):len(str2)]==str1:
+                return True
+        for i in range(0,len(words)):
+            for j in range(i+1,len(words)):
+                if isPrefixAndSuffix(words[i],words[j]):
+                    c+=1
+        return c
